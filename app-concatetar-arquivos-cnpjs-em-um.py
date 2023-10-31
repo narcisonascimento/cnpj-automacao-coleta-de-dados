4a47c0315c470af91f84d28b5e4f7622f4186dcd
Narciso Nascimento <nar.nasci@gmail.com>
2023-10-31 15:40:05 -0300

    atualização do código

---
+++ app-concatetar-arquivos-cnpjs-em-um.py
@@ -0,0 +1,39 @@
+import glob
+import pandas as pd
+import time
+
+# specifying the path to csv files
+path = './data/output/lista-cnpjs-separados'
+
+# csv files in the path
+file_list = glob.glob(path + "/*.xlsx")
+
+# list of excel files we want to merge.
+# pd.read_excel(file_path) reads the  
+# excel data into pandas dataframe.
+excl_list = []
+
+for file in file_list:
+    excl_list.append(pd.read_excel(file))
+
+# concatenate all DataFrames in the list
+# into a single DataFrame, returns new 
+# DataFrame.
+excl_merged = pd.concat(excl_list, ignore_index=True)
+
+# cleaning dataframe before export to excel
+excl_merged_complete = excl_merged.loc[(excl_merged['cnae'] != "cnpj nao encontrado")]
+excl_merged_complete = excl_merged_complete.drop_duplicates('cnpj').reset_index(drop=True)
+
+excl_merged_uncomplete = excl_merged.loc[(excl_merged['cnae'] == "cnpj nao encontrado")]
+excl_merged_uncomplete = excl_merged_uncomplete.drop_duplicates('cnpj').reset_index(drop=True)
+
+# exporting to excel file with date and time
+todays_date = time.strftime("%d-%m-%Y")
+todays_time = time.strftime("%H-%M-%S")
+excelfilename = "rnt-cnpj-com-cnae" + "-dia-" + todays_date + "-hora-" + todays_time +".xlsx"
+excelfilename_uncomplete = "rnt-cnpj-com-cnae" + "-dia-" + todays_date + "-hora-" + todays_time + "-sem-cnae"+".xlsx"
+
+
+excl_merged_complete.to_excel(f"data\output\{excelfilename}", sheet_name='sheet1', index=False)
+excl_merged_uncomplete.to_excel(f"data\output\{excelfilename_uncomplete}", sheet_name='sheet1', index=False)
