4c1cd98a463abc96427fc92c4e4ef3527bb2f322
Narciso Nascimento <nar.nasci@gmail.com>
2023-10-31 15:43:03 -0300

    Atualizando o código com comentários

--- app-cnpj.py
+++ app-cnpj.py
@@ -1,19 +1,17 @@
 # importando as bibliotecas
 
+import time
 import openpyxl
 import pandas as pd
-import time
-import random
+
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # arquivo com os CNPJs para criar a lista de CNPJs e montar a URL
-# df = pd.read_excel('data/output/bases-separadas/-7.xlsx')
-
-# lista de CNPJS
-df = pd.read_excel('data/output/bases-separadas/-0.xlsx')
-cnpj_lista = df['cnpj'].values.tolist() # lista final
+# aqui pode ser feito uma melhoria, onde o usuário pode colocar o nome do arquivo via CMD sem ter que abrir o vscode
+df = pd.read_excel('data/output/bases-separadas/-41.xlsx')
+cnpj_lista = df['cnpj'].values.tolist()  # lista final
 
 # teste de CNPJ
 # cnpj_lista = ['29614931000161'] # página teste
@@ -23,7 +21,7 @@ cnpj_lista = df['cnpj'].values.tolist() # lista final
 
 cnpj_lista_verificado = []
 
-# acessar o site por uma lista de URLS 
+# acessar o site por uma lista de URLS
 for i in list(cnpj_lista):
     try:
         driver = webdriver.Chrome()
@@ -31,15 +29,18 @@ for i in list(cnpj_lista):
         # url = f"https://amazon.com/"
         driver.get(url)
 
-        #tempo para carregar o popup da página
+        # tempo para carregar o popup da página
         time.sleep(7)
 
-        #clicar no botão do popup
-        driver.find_element(By.XPATH, "/html/body/div[3]/div[1]/div/div/div[2]/button[2]").click()
+        # clicar no botão do popup
+        driver.find_element(
+            By.XPATH, "/html/body/div[3]/div[1]/div/div/div[2]/button[2]").click()
 
         # extrair os dados de cnpj e cnae e colocar na lista
-        dado_cnpj = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[1]/p[1]/span[2]/b").text
-        dado_cnae = driver.find_element(By.XPATH, "//span[contains(@style,'cursor: pointer;')]").text
+        dado_cnpj = driver.find_element(
+            By.XPATH, "/html/body/div[1]/div/div/div/div[1]/p[1]/span[2]/b").text
+        dado_cnae = driver.find_element(
+            By.XPATH, "//span[contains(@style,'cursor: pointer;')]").text
 
         # colocar os dados na lista
         cnpj_lista_verificado.append([dado_cnpj, dado_cnae])
@@ -50,15 +51,15 @@ for i in list(cnpj_lista):
         dado_cnae = "cnpj nao encontrado"
         cnpj_lista_verificado.append([dado_cnpj, dado_cnae])
 
-
     # print(cnpj_lista_verificado)
     driver.quit()
 
     # tempo de pausa entre consultas
-    time.sleep(3)
+    time.sleep(1)
 
 # tratar os dados antes de exportar
-df_cnpj_verificado = pd.DataFrame(cnpj_lista_verificado, columns=['cnpj', 'cnae']).drop_duplicates().reset_index(drop=True)
+df_cnpj_verificado = pd.DataFrame(cnpj_lista_verificado, columns=[
+                                  'cnpj', 'cnae']).drop_duplicates().reset_index(drop=True)
 df['cnpj'] = df['cnpj'].astype('object')
 df.loc[:, 'cnae'] = df_cnpj_verificado['cnae']
 df['cnae'] = df['cnae'].astype('str')
@@ -68,7 +69,9 @@ df['cnae'] = df['cnae'].str.replace("</u>", "")
 # Criando o nome do arquivo antes de exportar
 todays_date = time.strftime("%d-%m-%Y")
 todays_time = time.strftime("%H-%M-%S")
-excelfilename = "rnt-cnpj-com-cnae" + "-dia-" + todays_date + "-hora-" + todays_time +".xlsx"
+excelfilename = "rnt-cnpj-com-cnae" + "-dia-" + \
+    todays_date + "-hora-" + todays_time + ".xlsx"
 
 # Exportando o arquivo para o excel
-df.to_excel(f"data\output\lista-cnpjs-separados\{excelfilename}", sheet_name='sheet1', index=False)
+df.to_excel(
+    f"data\output\lista-cnpjs-separados\{excelfilename}", sheet_name='sheet1', index=False)
